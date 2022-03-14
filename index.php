3ed53a908b6390873e125a5d694ffece1dfdc10b
Alexis <alexis.escudero195@gmail.com>
2022-03-14 19:19:34 +0100

    l

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require "./vendor/autoload.php"; 
 
 // chargement de flight 
-$loader = new \Twig\Loader\FilesystemLoader(dirname(__FILE__) . '/~aescuderviews');
+$loader = new \Twig\Loader\FilesystemLoader(dirname(__FILE__) . '/~aescuder/views');
 $twigConfig = array(
     // 'cache' => './cache/twig/',
     // 'cache' => false,
